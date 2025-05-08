133ea4f570472ab11238c279bce64db6b61de4cb
박종범 <jbpark03@gmail.com>
2025-05-08 12:26:12 +0900

    final css and dic class

--- src/App.jsx
+++ src/App.jsx
@@ -17,20 +17,18 @@ function App() {
 
   return (
     <>
-      <div>
-        <h1>색 선택기</h1>
-        <div>{
-          colors.map((color, idx) => (
-            <div key={idx} className="color-block"
-              style={{ backgroundColor: color }}
-              onClick={() => handleClick(color)} />
-          ))
-        }</div>
-        <div className="custom-color-picker">
-          <input type="color" value={bgColor} 
-            onChange={(e) => handleClick(e.target.value)} />
-        </div>
-      </div>
+<h1>색 선택기</h1>
+<div className="color-palette">{
+  colors.map((color, idx) => (
+    <div key={idx} className="color-block"
+      style={{ backgroundColor: color }}
+      onClick={() => handleClick(color)} />
+  ))
+}</div>
+<div className="custom-color-picker">
+  <input type="color" value={bgColor}
+    onChange={(e) => handleClick(e.target.value)} />
+</div>
     </>
   )
 }
