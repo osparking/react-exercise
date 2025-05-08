9e2b88458919c5f70b29890cd8d918f9f2a001f1
박종범 <jbpark03@gmail.com>
2025-05-08 15:33:51 +0900

    just prints 'useEffect 후크 시험'

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,10 @@
-import { ColorPicker } from "primereact/colorpicker";
-import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  const colors = ['#ffffcc', '#33ccff', '#66ff66', '#ffcc66', '#ff66ff'];
-  const [bgColor, setBgColor] = useState(colors[0]);
-
-  function handleClick(color) {
-    setBgColor(color);
-    document.body.style.backgroundColor = color;
-  }
-
-  useEffect(() => {
-    document.body.style.backgroundColor = bgColor;
-  }, []);
 
   return (
     <>
-<h1>색 선택기</h1>
-<div className="color-palette">{
-  colors.map((color, idx) => (
-    <div key={idx} className="color-block"
-      style={{ backgroundColor: color }}
-      onClick={() => handleClick(color)} />
-  ))
-}</div>
-<div className="custom-color-picker">
-  <input type="color" value={bgColor}
-    onChange={(e) => handleClick(e.target.value)} />
-</div>
+      <h1>useEffect 후크 시험</h1>
     </>
   )
 }
