717ff404ddcdd690da44c94945c4acf35d78bb4d
박종범 <jbpark03@gmail.com>
2025-05-10 05:56:01 +0900

    init - before move focus with useRef

--- src/App.jsx
+++ src/App.jsx
@@ -3,27 +3,10 @@ import './App.css';
 
 function App() {
   const refCnt = useRef(0);
-  const [stateCnt, setStateCnt] = useState(0);
-
-  useEffect(()=>{
-    console.log("리렌더");    
-  })
-  
-  const incStateCnt = () => {
-    setStateCnt(stateCnt + 1);
-  }
-  const incRefCnt = () => {
-    refCnt.current += 1;
-    console.log("refCnt: ", refCnt.current);
-  }
 
   return (
-    <div>
-      <p>상태 갯수: {stateCnt}</p>
-      <button onClick={incStateCnt}>상태 증가</button>
-      <br/>
-      <p>참조 갯수: {refCnt.current}</p>
-      <button onClick={incRefCnt}>참조 증가</button>
+    <div className="mt-5">
+      <h1>리액트 연습</h1>
     </div>
   )
 }
