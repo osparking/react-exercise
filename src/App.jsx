64dc92ccb586c71712c128d6c73ef3f3fe784d7c
박종범 <jbpark03@gmail.com>
2025-05-10 06:14:50 +0900

    adds second set of input and button

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,10 @@ function App() {
   const setFocusBox1 = () => {
     inputTop.current.focus();
   }
+  const inputBottom = useRef(null);
+  const setFocusBox2 = () => {
+    inputBottom.current.focus();
+  }
 
   return (
     <div className="mt-5">
@@ -14,6 +18,10 @@ function App() {
       <input type="text" ref={inputTop} placeholder='상자 1'/>
       <br/>
       <button onClick={setFocusBox1}>상자 1 입력</button>
+      <br/>
+      <input type="text" ref={inputBottom} placeholder='상자 2'/>
+      <br/>
+      <button onClick={setFocusBox2}>상자 2 입력</button>
     </div>
   )
 }
