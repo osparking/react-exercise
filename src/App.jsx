80c9faeb2081be98ac590fe9e870757a2bb840e4
박종범 <jbpark03@gmail.com>
2025-05-09 17:53:57 +0900

    App contains Parent

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,10 @@
-import { useEffect, useState } from 'react';
 import './App.css';
+import Parent from './Parent';
 
 function App() {
-  const [clock, setClock] = useState(new Date());
-useEffect(() => {
-  setInterval(() => {
-    setClock(new Date());
-  }, 1000)
-}, []);
-
   return (
     <div>
-      <h1>영원한 흐름</h1>
+      <Parent/>
     </div>
   )
 }
