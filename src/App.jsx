71e617608cf097bbfcc33552bb8184238a4d8967
박종범 <jbpark03@gmail.com>
2025-05-10 06:22:55 +0900

    revert background color on blur

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,28 @@ function App() {
   const inputTop = useRef(null);
   const setFocusBox1 = () => {
     inputTop.current.focus();
+    inputTop.current.style.backgroundColor = "pink";
   }
   const inputBottom = useRef(null);
   const setFocusBox2 = () => {
     inputBottom.current.focus();
+    inputBottom.current.style.backgroundColor = "pink";
+  }
+  const focusLost = (e) => {
+    e.target.style.backgroundColor = "white";
   }
 
   return (
     <div className="mt-5">
       <h1>리액트 연습</h1>
       <br/>
-      <input type="text" ref={inputTop} placeholder='상자 1'/>
+      <input type="text" ref={inputTop} placeholder='상자 1' 
+        onBlur={focusLost}/>
       <br/>
       <button onClick={setFocusBox1}>상자 1 입력</button>
       <br/>
-      <input type="text" ref={inputBottom} placeholder='상자 2'/>
+      <input type="text" ref={inputBottom} placeholder='상자 2'
+        onBlur={focusLost}/>
       <br/>
       <button onClick={setFocusBox2}>상자 2 입력</button>
     </div>
