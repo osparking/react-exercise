8c0321563948746711534a980bdd974cb9775f44
박종범 <jbpark03@gmail.com>
2025-05-10 06:10:02 +0900

    useRef - input, button, focus

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,18 @@ import { useEffect, useRef, useState } from 'react';
 import './App.css';
 
 function App() {
-  const refCnt = useRef(0);
+  const inputTop = useRef(null);
+  const setFocusBox1 = () => {
+    inputTop.current.focus();
+  }
 
   return (
     <div className="mt-5">
       <h1>리액트 연습</h1>
+      <br/>
+      <input type="text" ref={inputTop} placeholder='상자 1'/>
+      <br/>
+      <button onClick={setFocusBox1}>상자 1 입력</button>
     </div>
   )
 }
