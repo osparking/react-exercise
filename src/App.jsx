584e0f205f6de4c70f8c8ce2cfae692412394cd3
박종범 <jbpark03@gmail.com>
2025-05-09 10:37:18 +0900

    just print out welcome message

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,10 @@
-import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [value, setValue] = useState(0);
-
-  const incCount = () => {
-    setCount(count + 1);
-    document.title = `계수기:${count}`;
-  }
-
-  const incValue = () => {
-    setValue(value + 1);
-    console.log("value inc'ed");
-  }
-
-  useEffect(() => {
-    document.title = `계수기:${count}`;
-    console.log("useEffect called");
-  });
-
+  
   return (
     <>
-      <h1>useEffect 후크 시험</h1>
-      <button onClick={incCount}>증가</button>
-      <button onClick={incValue}>증가</button>
+      <h1>환영합니다.</h1>
     </>
   )
 }
