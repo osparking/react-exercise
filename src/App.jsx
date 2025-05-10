86882b3354dc6302d9465e942eb7d24cbb63289d
박종범 <jbpark03@gmail.com>
2025-05-10 12:09:09 +0900

    surround return value w/ *.Provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-import { createContext, useContext } from 'react';
+import { createContext } from 'react';
 import './App.css';
 import FlavouredCompo from './FlavouredCompo';
 
 function CompoA() {
   return (
-    <div className="mt-5"
-      style={{ border: "2px solid black", padding: "20px" }}>
-      <h2>CompoA (아버지)</h2>
-      <FlavouredCompo />
-    </div>
+<FlavourContext.Provider value='바닐라'>
+  <div className="mt-5"
+    style={{ border: "2px solid black", padding: "20px" }}>
+    <h2>CompoA (아버지)</h2>
+    <FlavouredCompo />
+  </div>
+</FlavourContext.Provider>
   )
 }
 
