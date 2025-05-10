d0ba816500a5c3bdfbf28289db779fc9b93708b5
박종범 <jbpark03@gmail.com>
2025-05-10 15:21:23 +0900

    flavour changer button for CompoB in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,25 @@
-import { createContext } from 'react';
+import { createContext, useState } from 'react';
 import './App.css';
 import CompoA from './CompoA';
 import CompoB from './CompoB';
 
-export const FlavourContext = createContext("초콜릿");
-
 function App() {
-
+  const [flavour, setFlavour] = useState("초콜릿");
+  const changeFlavour = () => {
+    setFlavour(flavour === "초콜릿" ? "플레인" : "초콜릿");
+  }
   return (
-    <div className="mt-5"
-      style={{ border: "2px solid black", padding: "20px" }}>
-      <h2>App (할아버지)</h2>
-      <CompoA />
-      <CompoB />
-    </div>
+    <FlavourContext.Provider value={flavour}>
+      <div className="mt-5"
+        style={{ border: "2px solid black", padding: "20px" }}>
+        <h2>App (할아버지)</h2>
+        <CompoA />
+        <button onClick={changeFlavour}>맛 변경</button>
+        <CompoB />
+      </div>
+    </FlavourContext.Provider>
   )
 }
 
+export const FlavourContext = createContext("초콜릿");
 export default App;
