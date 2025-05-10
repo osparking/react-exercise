7efb8ec50ee0f40fc39edc349555b91ccc83053a
박종범 <jbpark03@gmail.com>
2025-05-10 14:55:35 +0900

    insert flavour changer button

--- src/CompoA.jsx
+++ src/CompoA.jsx
@@ -1,8 +1,15 @@
+import { useState } from 'react';
 import { FlavourContext } from './App';
 import FlavouredCompo from './FlavouredCompo';
 const CompoA = () => {
-  return (
-    <FlavourContext.Provider value='녹차'>
+  const [flavour, setFlavour] = useState("플레인");
+  const changeFlavour = () => {
+    setFlavour(flavour === "바닐라" ? "플레인" : "바닐라");
+  }
+
+  return (    
+    <FlavourContext.Provider value={flavour}>
+      <button onClick={changeFlavour}>맛 변경</button>
       <div className="mt-5"
         style={{ border: "2px solid black", padding: "20px" }}>
         <h2>CompoA (아버지)</h2>
