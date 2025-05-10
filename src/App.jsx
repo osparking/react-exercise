bb364b7ea2a4c2755ce6d868a983a667d92dab74
박종범 <jbpark03@gmail.com>
2025-05-10 10:12:36 +0900

    apply same styles to parent and myself

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
 import './App.css';
 
 function FlavouredCompo({ flavour }) {
-  return <div>아이스크림 맛: {flavour}</div>
+  return (
+    <div className="mt-5"
+      style={{ border: "2px solid black", padding: "20px" }}>
+      <h2>FlavouredCompo (나)</h2>
+      아이스크림 맛: {flavour}
+    </div>
+  )  
 }
 
 function CompoA({ flavour }) {
-  return <FlavouredCompo flavour={flavour} />;
+  return (
+    <div className="mt-5"
+      style={{ border: "2px solid black", padding: "20px" }}>
+      <h2>CompoA (아버지)</h2>
+      <FlavouredCompo flavour={flavour} />
+    </div>
+  )  
 }
 
 function App() {
